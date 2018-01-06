1b59021c71360bd4327a16f82bd793100476bc49
Gabriel Naves da Silva <gabri.navess@gmail.com>
2018-01-06 15:27:19 -0200

    Adding camera follow

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    GameObject player;
+
+    void Update() {
+        if (!player)
+            LookForPlayer();
+        else
+            FollowPlayer();
+    }
+
+    void LookForPlayer() {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void FollowPlayer() {
+        var position = player.transform.position;
+        position.z = -10;
+        transform.position = position;
+    }
+}
