4bc142fe1f489a7ede78053dd812ba2f13ee8741
Gabriel Naves da Silva <gabri.navess@gmail.com>
2018-01-07 06:18:19 -0200

    Implementing menu and scene changing

---
+++ Assets/Scripts/LevelChange/GameStarter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStarter : MonoBehaviour {
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+        else if (Input.anyKeyDown) {
+            GetComponent<LevelChanger>().StartGame();
+        }
+    }
+}
---
+++ Assets/Scripts/LevelChange/LevelChanger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour {
+
+    public string baseLevelKey = "level";
+    public string menuScene = "menu";
+    public string levelScene = "level";
+
+    public void StartGame() {
+        PlayerPrefs.SetInt("CurrentLevel", 1);
+        SceneManager.LoadScene(levelScene);
+    }
+
+    public void RestartCurrentScene() {
+        SceneManager.LoadScene(levelScene);
+    }
+
+    public void GoToNextLevel() {
+        int nextLevel = PlayerPrefs.GetInt("CurrentLevel") + 1;
+        string nextJSONFile = baseLevelKey + nextLevel;
+        if (Resources.Load(nextJSONFile) as TextAsset != null) {
+            PlayerPrefs.SetInt("CurrentLevel", nextLevel);
+            SceneManager.LoadScene(levelScene);
+        }
+        else {
+            SceneManager.LoadScene(menuScene);
+        }
+    }
+}
